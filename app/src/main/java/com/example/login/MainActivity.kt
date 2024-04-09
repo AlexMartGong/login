791be6cf700d42750a7fd486e9f2e0a6d9220b35
AlexMartGong <155708055+AlexMartGong@users.noreply.github.com>
2024-04-09 09:57:24 -0600

    Login ready

--- app/src/main/java/com/example/login/MainActivity.kt
+++ app/src/main/java/com/example/login/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.login
 
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    val user = "AlexMG"
+    val pass = "12345"
+
+    lateinit var txtUser: EditText
+    lateinit var txtPassword: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +26,17 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        txtUser = findViewById(R.id.txtUser)
+        txtPassword = findViewById(R.id.txtPass)
+    }
+
+    fun clickItem(v: View) {
+
+        if (user == txtUser.text.toString() && pass == txtPassword.text.toString()) {
+            Toast.makeText(this, "Login", Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this, "Error ${txtUser.text.toString()} y ${txtPassword.text.toString()}", Toast.LENGTH_LONG).show()
         }
     }
 }
